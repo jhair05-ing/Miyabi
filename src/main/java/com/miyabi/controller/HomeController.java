403512daaa9b411446e4374925db7cef96ef8eb3
Jhair <jhair.mc.0@gmail.com>
2026-02-27 00:57:56 -0500

    avance 2

--- src/main/java/com/miyabi/controller/HomeController.java
+++ src/main/java/com/miyabi/controller/HomeController.java
@@ -2,10 +2,16 @@ package com.miyabi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import com.miyabi.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller 
 public class HomeController {
 
+	@Autowired
+    private ReservationService reservationService;
+	
     // Ruta para la página principal 
     @GetMapping("/")
     public String index() {
@@ -21,4 +27,16 @@ public class HomeController {
     public String cuisine() {
         return "cuisine"; 
     }
-}
+ // 5. AGREGA ESTA RUTA NUEVA
+    @GetMapping("/reservas")
+    public String reservas(Model model) {
+        try {
+            model.addAttribute("reservations", reservationService.findAll());
+            return "reservas";
+        } catch (Exception e) {
+            System.err.println("ERROR AL CARGAR RESERVAS: " + e.getMessage());
+            e.printStackTrace();
+            return "error"; // Si tienes una página de error, o solo deja que explote para ver la consola
+        }
+    }
+}    
